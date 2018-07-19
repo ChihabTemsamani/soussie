673fee57f3b7f9e4167d851a6028f7ecef327c1f
ikmarokko2 <35876458+ikmarokko2@users.noreply.github.com>
2018-07-19 12:31:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('My dreams', {type: 'WATCHING'});
+    client.user.setActivity('GewoneVrienden.nl', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
